c36af08a0b868b2443d2d102378fa93f5852c4ed
Dominik <zawlockidominik@gmail.com>
2018-10-29 21:08:08 +0100

    basic declaration

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+// A sudoku 
+int sud[9][9];
+
+void generateSudoku() {
+	
+	for(int xx=0; xx<9; xx++) {
+		for(int yy=0; yy<9; yy++) {
+			
+			
+			
+		}
+	}
+}
+
 int main() {
 
 	cout << "Hello, my own, small, black world..." << endl;
