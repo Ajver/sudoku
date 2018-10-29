1141db9ccbf2ef06b885d66489bbee0239aecf79
Dominik <zawlockidominik@gmail.com>
2018-10-29 21:21:11 +0100

    Add get full vector function (it returns us vector<int> with number from 1 to 9

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 // A sudoku 
 int sud[9][9];
 
+vector<int> getFullVector();
+
 void generateSudoku() {
-	
-	for(int xx=0; xx<9; xx++) {
+
+	for(int xx=0; xx<9; xx++) { 
 		for(int yy=0; yy<9; yy++) {
-			
-			
 			
 		}
 	}
@@ -19,6 +20,16 @@ void generateSudoku() {
 int main() {
 
 	cout << "Hello, my own, small, black world..." << endl;
-	
+		
+	generateSudoku();	
+		
 	return 0;
+}
+
+vector<int> getFullVector() {
+	vector<int> v;
+	for(int i=1; i<=9; i++) {
+		v.push_back(i);
+	}
+	return v;
 }
