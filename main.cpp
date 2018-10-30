67ea1d3f22cb41f6924a5e0601cdce462a8d0aae
Dominik <zawlockidominik@gmail.com>
2018-10-30 13:00:24 +0100

    Not realy working algorithm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <time.h>
 #include <vector>
 
 using namespace std;
+
+// Helping structure
+struct square {
+	int x;
+	int y;
+};
 
 // A sudoku 
 int sud[9][9];
@@ -15,7 +22,9 @@ void printSudoku();
 // Function that erasing from vector specific value (not from specyific place)
 void removeFromVec(vector<int> &v, int what);
 
-void generateSudoku() {
+square getSquare(int x, int y);
+
+bool generateSudoku() {
 	
 	for(int xx=0; xx<9; xx++) { 
 		for(int yy=0; yy<9; yy++) {
@@ -25,12 +34,39 @@ void generateSudoku() {
 			// Checking in row
 			for(int i=0; i<9; i++) {
 				if(sud[i][yy] != 0) {
-					
+					removeFromVec(v, sud[i][yy]);
 				}
 			}
 			
+			// Checking in column
+			for(int i=0; i<9; i++) {
+				if(sud[xx][i] != 0) {
+					removeFromVec(v, sud[xx][i]);
+				}
+			}
+			
+			// Checking in squre
+			square s = getSquare(xx, yy);
+			
+			for(int fx=s.x*3; fx<s.x*3+3; fx++) {
+				for(int fy=s.y*3; fy<s.y*3+3; fy++) {
+					if(sud[fx][fy] != 0) {
+						removeFromVec(v, sud[fx][fy]);
+					}
+				}
+			} 
+			
+			if(v.size() > 0) {
+				int nr = rand() % v.size();
+				int val = v[nr];
+				sud[xx][yy] = val;
+			}else {
+				return false;
+			}
 		}
 	}
+	
+	return true;
 }
 
 
@@ -38,8 +74,19 @@ void generateSudoku() {
 
 int main() {
 
-	//generateSudoku();
+	srand(time(NULL));
+
+	/*
+	int counter = 0;
 	
+	while(!generateSudoku()) {
+		counter++;
+	}
+	
+	cout << "After " << counter << " times:" << endl;
+	*/
+	
+	generateSudoku();
 	printSudoku();
 	
 	return 0;
@@ -60,7 +107,12 @@ void printSudoku() {
 	for(int yy=0; yy<9; yy++) { 
 		cout << "      ";
 		for(int xx=0; xx<9; xx++) {
-			cout << sud[xx][yy] << " ";				
+			if(sud[xx][yy] != 0) {
+				cout << sud[xx][yy];
+			}else {
+				cout << " ";
+			}
+			cout << " ";			
 		}
 		cout << endl;
 	}
@@ -73,6 +125,17 @@ void removeFromVec(vector<int> &v, int what) {
 			v.erase(v.begin() + i);
 		}
 	}
+}
+
+square getSquare(int x, int y) {
+	int a = x/3;
+	int b = y/3;
+	
+	square s;
+	s.x = a;
+	s.y = b;
+	
+	return s;
 }
 
 
